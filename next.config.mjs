975478a9178c00f20dd52060607b60a762eeeb20
diandwi3 <22560017@stmik.jayakarta.ac.id>
2024-07-12 19:37:36 +0700

    update

---
+++ next.config.mjs
@@ -0,0 +1,10 @@
+"use client";
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {images: {
+    domains: ['substackcdn.com', 'flixable.b-cdn.net', 'https://m.media-amazon.com/']
+}
+
+};
+
+export default nextConfig;
